9f4a36568d6b356a68cacddaff27fb3a31bf3945
ARahmanLskit <arahman@lskit.com>
2023-07-25 12:15:06 +0600

    token verify done

--- index.js
+++ index.js
@@ -99,7 +99,21 @@ const run = async () => {
       }
     });
 
-    
+    // Secure your APIs with JWT authentication middleware
+    const verifyToken = (req, res, next) => {
+      const token = req.header("Authorization")?.replace("Bearer ", "");
+      if (!token) {
+        return res.status(401).json({ error: "Access denied. Token missing." });
+      }
+
+      try {
+        const decoded = jwt.verify(token, secretKey);
+        req.user = decoded;
+        next();
+      } catch (error) {
+        res.status(401).json({ error: "Invalid token." });
+      }
+    };
 
     // post data
 
